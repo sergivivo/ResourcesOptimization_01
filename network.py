Fixed normalization and first ILP test

network.py
@@ -453,52 +453,55 @@ if __name__ == '__main__':
 
     ntw = Network(configs)
 
-    matrix = np.zeros((configs.n_tasks, configs.n_nodes), np.uint8)
-    for row in range(configs.n_tasks):
-        col = random.randrange(configs.n_nodes)
-        matrix[row, col] = 1
+    print(ntw.getTaskMemoryArray())
+    print(ntw.getNodeMemoryArray())
 
-    tuam = ntw.getTaskUserAssignmentMatrix()
-    tudm = ntw.getTaskUserDistanceMatrix(matrix, includeAll=False)
+    #matrix = np.zeros((configs.n_tasks, configs.n_nodes), np.uint8)
+    #for row in range(configs.n_tasks):
+    #    col = random.randrange(configs.n_nodes)
+    #    matrix[row, col] = 1
 
-    tua_sum = np.sum(tudm, axis=1) 
-    tud_sum = np.sum(tuam, axis=1)
-    tu_filter = (tua_sum != 0) # Filter so that there's no division by zero
+    #tuam = ntw.getTaskUserAssignmentMatrix()
+    #tudm = ntw.getTaskUserDistanceMatrix(matrix, includeAll=False)
+
+    #tua_sum = np.sum(tudm, axis=1) 
+    #tud_sum = np.sum(tuam, axis=1)
+    #tu_filter = (tua_sum != 0) # Filter so that there's no division by zero
 
 
-    print(tuam)
-    print(tudm)
-    print()
+    #print(tuam)
+    #print(tudm)
+    #print()
 
-    print(tua_sum)
-    print(tud_sum)
-    print()
+    #print(tua_sum)
+    #print(tud_sum)
+    #print()
 
-    print(tua_sum[tu_filter])
-    print(tud_sum[tu_filter])
-    print()
+    #print(tua_sum[tu_filter])
+    #print(tud_sum[tu_filter])
+    #print()
 
-    avg_row = tua_sum[tu_filter] / tud_sum[tu_filter]
+    #avg_row = tua_sum[tu_filter] / tud_sum[tu_filter]
 
-    print(avg_row)
-    print()
-    
-    print(np.average(avg_row))
-    print()
+    #print(avg_row)
+    #print()
+    #
+    #print(np.average(avg_row))
+    #print()
 
-    capacity = ntw.getNodeMemoryArray()
-    occupied = ntw.getNodeOccupiedMemoryArray(matrix)
-    print(capacity)
-    print(occupied)
-    print()
-    print(ntw.getNodeAvailableMemoryArray(matrix))
-    print()
+    #capacity = ntw.getNodeMemoryArray()
+    #occupied = ntw.getNodeOccupiedMemoryArray(matrix)
+    #print(capacity)
+    #print(occupied)
+    #print()
+    #print(ntw.getNodeAvailableMemoryArray(matrix))
+    #print()
 
-    print('=====================================')
-    print('Testing task/user assignment function')
-    print('=====================================')
-    print()
+    #print('=====================================')
+    #print('Testing task/user assignment function')
+    #print('=====================================')
+    #print()
 
-    print(ntw._generateTaskUserAssignmentMatrix_v2())
+    #print(ntw._generateTaskUserAssignmentMatrix_v2())
 
 

problems.py
@@ -162,10 +162,11 @@ class Problem01v3(ElementwiseProblem):
         self.multimode = multimode
         self.l = l # lambda for converting bidimensional to single
 
+        # Values needed for normalization
         self.f1_min = 0.
-        self.f1_max = 1000.
+        self.f1_max = np.max(network.getUserNodeDistanceMatrix())
         self.f2_min = 0.
-        self.f2_max = 1000.
+        self.f2_max = self.N_NODES
 
         super().__init__(
                 n_var = 1,
@@ -174,13 +175,9 @@ class Problem01v3(ElementwiseProblem):
 
 
     def _evaluate(self, x, out, *args, **kwargs):
-        # En cada generación, guardar el máximo y el mínimo f1 y f2
-        # https://pymoo.org/interface/callback.html
-        # TODO: hacer unas ejecuciones, ver cuál es el posible mayor y menor valor que se pueda
-        #       obtener en la solución, requiere de una ejecución previa, pasar por argumento
         matrix = x[0]
 
-        f1 = self.network.getTasksAverageDistanceToUser(matrix)
+        f1 = self.network.getTasksAverageDistanceToUser(matrix) 
         f2 = np.count_nonzero(np.max(matrix, axis=0))
 
         assigned_memory_v = np.sum(self.network.getTaskNodeMemoryMatrix(matrix), axis=0)
@@ -204,7 +201,7 @@ class Problem01v3(ElementwiseProblem):
 
 
 
-
+# Testing
 def solveAndAddToPlot(problem, algorithm, termination, name, color):
     res = minimize(
         problem,